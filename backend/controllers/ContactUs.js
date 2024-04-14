cd25ff5a95ebb5811b9d7bd8fc01bd77e9caa251
pantharshit007 <aaryanpandit3527@gmail.com>
2024-04-14 10:48:43 +0530

    ContactUs Controller+Mails

--- backend/controllers/ContactUs.js
+++ backend/controllers/ContactUs.js
@@ -1,15 +1,49 @@
-// TODO: CREATE A CONTROLLER WHERE ONCE USER FILLS HIS DETAIL
-// SEND 2 MAILS:
-// 1 TO THE STUDENT ABOUT MAIL RECEIVING CONFIRMATION
-// 2 TO THE MOD OR YOURSELF ABOUT THE QUERY OF STUDENT
-
 const mailSender = require("../utils/mailSender")
-const { contactUsEmail } = require('../mail/contactFormTemp')
+const { contactUsEmail } = require('../mail/contactForm')
 
 exports.contactUser = async (req, res) => {
     try {
+        const { email,
+            firstname,
+            lastname,
+            message,
+            phoneNo,
+        } = req.body;
+
+        if (!email || !message) {
+            return res.status(404).json({
+                success: false,
+                message: 'Fill the required fields.'
+            })
+        }
+
+        // Send Mail to student regarding Email confirmation
+        const titleStudent = "Mail Received"
+        const verificationMail = await mailSender(email, titleStudent, contactUsEmail(email, firstname, lastname, message, phoneNo))
+        console.log("verificationMail: " + verificationMail)
+
+        //send Mail to MOD 
+        const titleMod = "Contact Us Mail Details"
+        const modEmail = "modEmail@gmail.com"
+        const body = `
+            <h2>Student Contact Details</h2>
+            <p>Name: ${firstname} ${lastname}</p>
+            <p>Email: ${email}</p>
+            <p>Phone Number: ${phoneNo}</p>
+            <p>Message: ${message}</p>
+            `
+        const modMail = await mailSender(modEmail, titleMod, body);
+
+        return res.status(200).json({
+            success: true,
+            message: 'Email sent successfully'
+        })
 
     } catch (err) {
-
+        console.log("Failed to send email: " + err.message)
+        return res.status(500).json({
+            success: false,
+            message: "Failed to send email: " + err.message,
+        })
     }
 }
--- backend/controllers/Payments.js
+++ backend/controllers/Payments.js
@@ -2,7 +2,7 @@ const { instance } = require('../config/razorpay');
 const Course = require('../models/Course')
 const User = require('../models/User');
 const mailSender = require('../utils/mailSender');
-const { courseEnrollmentTemplate } = require('../mail/courseEnrollmentTemplate');
+const { courseEnrollmentTemplate } = require('../mail/courseEnrollment');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
