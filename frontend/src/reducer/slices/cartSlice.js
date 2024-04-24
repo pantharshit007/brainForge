959dc3ca488e444b1fb21cc7036c136ea03a9e2f
pantharshit007 <aaryanpandit3527@gmail.com>
2024-04-24 15:22:54 +0530

    todo: Done [cartSlice Reducers]

--- frontend/src/reducer/slices/cartSlice.js
+++ frontend/src/reducer/slices/cartSlice.js
@@ -5,6 +5,8 @@ const localTotalItems = localStorage.getItem('totalItems')  // fetching totalIte
 const localCart = localStorage.getItem('cart')  // fetching Itmes in Cart from LS
 const localTotal = localStorage.getItem('total')    // fetching total Price from LS
 
+const toastPostion = { position: "top-right" }
+
 const initialState = {
     totalItems: localTotalItems ? JSON.parse(localTotalItems) : 0,
     cart: localCart ? JSON.parse(localCart) : [],
@@ -12,21 +14,70 @@ const initialState = {
 }
 
 const cartSlice = createSlice({
-    name: 'slice',
+    name: 'cart',
     initialState,
     reducers: {
-        setTotalItem(state, value) {
-            state.token = value.payload;
-        },
-        // TODO: COMPLETE THEM
+
         //add to cart
-        addToCart: (state, action) => { },
+        addToCart: (state, action) => {
+            const course = action.payload;
+            const isAlreadyInCart = state.cart.findIndex(item => item._id === course._id);
+
+            // check if already in cart
+            if (isAlreadyInCart >= 0) {
+                toast.error("Course already in cart", toastPostion);
+                return
+            }
+
+            // add course to cart: arr[]
+            state.cart.push(course);
+            // update total Item and price
+            state.totalItems++;
+            state.total += course.price;
+
+            // update new values in Local storage
+            localStorage.setItem('cart', JSON.stringify(state.cart));
+            localStorage.setItem('totalItems', JSON.stringify(state.totalItems));
+            localStorage.setItem('total', JSON.stringify(state.total));
+
+            toast.success('Course added to cart', toastPostion);
+
+        },
 
         //remove from cart
-        removeFromCart: (state, action) => { },
+        removeFromCart: (state, action) => {
+            courseId = action.payload;
+            const coursePosition = state.cart.findIndex(item => item._id === courseId);
+
+            // check if course is available
+            if (coursePosition >= 0) {
+                // update cart, total Items and total Price
+                state.cart.splice(coursePosition, 1);  //coursePosition: position in array: 0,1,2,3,..
+                state.totalItems--;
+                state.total -= state.cart[coursePosition].price;
+
+                // update Local Storage
+                localStorage.setItem("cart", JSON.stringify(state.cart));
+                localStorage.setItem("totalItems", JSON.stringify(state.totalItems));
+                localStorage.setItem("total", JSON.stringify(state.total));
+
+                toast.success("Course Removed from cart", toastPostion);
+            }
+        },
 
         //reset cart
-        resetCart: (state) => { }
+        resetCart: (state) => {
+            // update all cart, total Items, total Price data to inital State
+            state.cart = [];
+            state.totalItems = 0;
+            state.total = 0;
+
+            // update Local storage data
+            localStorage.removeItem('cart');
+            localStorage.removeItem('total');
+            localStorage.removeItem('totalItems');
+
+        }
     }
 })
 
